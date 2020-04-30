c31a5bb9443d242499d81f90f0251f8bb3bb702c
nandyme <fernandomellone2@gmail.com>
2020-04-30 08:40:18 -0400

    se ocultaron los items del navbar en lg

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-item @click="logout" href="#">Cerrá sesión</b-nav-item>
+        <b-nav-item v-if="$route.meta.auth" @click="logout" href="#">Cerrá sesión</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
